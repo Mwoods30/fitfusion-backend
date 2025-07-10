4aef798cf5822a537f2f4e320d582f9fe7c06482
Matthew <mattwoods301@gmail.com>
2025-07-09 23:33:36 -0400

    add new yaml file and chagnge app.py to no be used locallly on render

--- server/app.py
+++ server/app.py
@@ -95,5 +95,7 @@ def history():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host ='0.0.0.0', port=5000)
+# Note: In production, set debug=False and use a proper WSGI server like Gunicorn.
+# Example: gunicorn app:app --bind
 
