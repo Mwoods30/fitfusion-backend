510977ffd504847566b5f442bcdc0a3e7fb94db7
Matthew <mattwoods301@gmail.com>
2025-07-09 23:24:18 -0400

    add render config for deployment

--- server/models.py
+++ server/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    workouts = db.relationship('WorkoutHistory', backref='user', lazy=True)
 
     def __repr__(self):
         return f"<User {self.email}>"
