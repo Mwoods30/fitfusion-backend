91212e1325224203f9ec18cefe09dd41a46a0351
Mwoods30 <128528136+Mwoods30@users.noreply.github.com>
2025-07-13 23:52:46 +0000

    fix db

--- server/models.py
+++ server/models.py
@@ -6,9 +6,9 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    name = db.Column(db.String(100), nullable=False)  # Added
+    name = db.Column(db.String(120))  # <- this was added later
     password = db.Column(db.String(200), nullable=False)
-
+    
 class WorkoutHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
